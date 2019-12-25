7d3ada8458b72a6e9e302a178f8f1c36b74111cc
Angel Garbarino <argarbarino@gmail.com>
2019-12-25 13:17:28 -0700

    setting up protein template

---
+++ app/components/protein-counter.js
@@ -0,0 +1,12 @@
+import Component from "@glimmer/component";
+import { tracked } from "@glimmer/tracking";
+import { action } from "@ember/object";
+
+export default class Counter extends Component {
+  @tracked count = 0;
+  
+  @action
+  increment(value) {
+    this.count = this.count + Number(value);
+  }
+}
--- app/router.js
+++ app/router.js
@@ -7,6 +7,7 @@ const Router = EmberRouter.extend({
 });
 
 Router.map(function() {
+  this.route('protein');
 });
 
 export default Router;
