caf3c011c45a23afcd9326b092b98e9d3b9d18c1
Angel Garbarino <argarbarino@gmail.com>
2020-01-26 15:17:40 -0700

    get average in there, and clean up

--- app/routes/protein.js
+++ app/routes/protein.js
@@ -1,18 +1,12 @@
-
-import Route from '@ember/routing/route';
-import moment from 'moment';
-import { hash } from 'rsvp';
+import Route from "@ember/routing/route";
+import moment from "moment";
+import { hash } from "rsvp";
 
 export default Route.extend({
-
   model() {
-    let today = moment().format("MMDDYYYY")
-    
     return hash({
-      blah: "blah",
-      fullRouteModel: this.store.findAll('protein'),
+      fullRouteModel: this.store.findAll("protein")
       // proteinForToday: this.store.findRecord('protein', today).then(v => v.proteinTotal),
-    })
+    });
   }
 });
-
